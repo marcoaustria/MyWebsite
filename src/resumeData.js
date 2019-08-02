e88fcf53f75a6086956f220a6fc836198c9d2331
Marco Austria <maustria0507@gmail.com>
2019-08-01 22:11:19 -0400

    first iter of react website

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,107 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Marco Austria",
+  "role": "",
+  "linkedinId": "marcoaustria",
+  "roleDescription": "Software Engineer, with an Eye for Design~",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/marcoaustria/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/marcoaustria",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "I am a Senior studying Computer Science in the Herbert Wertheim College of Engineering at the University of Florida. I am interested in mobile and web development for the purpose of helping people with their technological outreach, as well as the UX/UI/HCI aspects of these disciplines. I am skilled in multiple spoken languages, as well as multiple programming languages, and hope to learn many more in the future. My hobbies include traveling, learning languages, playing instruments, reading, and learning about new and upcoming technologies. I hope to continually learn more about Computer Science in class and out of class in internships to gain more experience and abilities, creating products that will be useful and impacting for the masses.",
+  "address": "Gainesville, FL",
+  "email": "",
+  "education": [
+    {
+      "UniversityName": "University of Florida",
+      "specialization": "B.S. in Computer Science(Engineering)",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2020",
+      "Achievements": "Minor in Digital Arts and Sciences"
+    },
+    {
+      "UniversityName": "University of New South Wales",
+      "specialization": "Sydney, Australia",
+      "MonthOfPassing": "Spring",
+      "YearOfPassing": "2017",
+      "Achievements": 'Study Abroad'
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "American Express Co.",
+      "specialization": "Software Engineering Intern",
+      "MonthOfLeaving": "Summer",
+      "YearOfLeaving": "2019",
+      "Achievements": "Automation, Python, FrontEnd, React, Flask API"
+    },
+    {
+      "CompanyName": "A Plus Home Rentals",
+      "specialization": "Office Assistant/Handyman",
+      "MonthOfLeaving": "",
+      "YearOfLeaving": "2012-2019",
+      "Achievements": "Automation, Java, Computer Maintenance & Optimization"
+    }
+  ],
+  "skillsDescription": "",
+  "skills": [
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Java"
+    },
+    {
+      "skillname": "HTML/CSS"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "EnglishAide",
+      "description": "React Native App",
+      "imgurl": "images/englishaide.png",
+      "giturl": "https://github.com/marcoaustria/EnglishAide"
+    },
+    {
+      "name": "Culture Shock",
+      "description": "Android App",
+      "imgurl": "images/cultureshock.png",
+      "giturl": ""
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png",
+      "giturl": ""
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg",
+      "giturl": ""
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
