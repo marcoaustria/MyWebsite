a4467ceae3814419e82f07b82bb9ca688397531b
Marco Austria <maustria0507@gmail.com>
2019-08-08 22:43:30 -0400

    Updates

---
+++ precache-manifest.a7bcef74ec50adb2979247113fe43a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3fcfdb1f97df2155cf21",
+    "url": "/static/js/main.3fcfdb1f.chunk.js"
+  },
+  {
+    "revision": "77c0f4afe363088e2d70",
+    "url": "/static/js/1.77c0f4af.chunk.js"
+  },
+  {
+    "revision": "3fcfdb1f97df2155cf21",
+    "url": "/static/css/main.b16ad837.chunk.css"
+  },
+  {
+    "revision": "3efe1026504a1fd01b41fad81c0692dd",
+    "url": "/index.html"
+  }
+];
